f2f437c13892fc57d49eea012f49e65db21714b8
yunjunseong77-cell <yun.junseong77@gmail.com>
2026-06-04 14:11:06 +0900

    Update song-key.js

--- api/song-key.js
+++ api/song-key.js
@@ -1,4 +1,6 @@
+// v2
 export default async function handler(req, res) {
+  export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   if (req.method === "OPTIONS") return res.status(200).end();
 
